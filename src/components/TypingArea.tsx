5ee4e30fcec103a600873e7657f7ad014d3ac125
Brendan Lawless <brendanlawless@hotmail.com>
2026-07-24 20:50:44 +0100

    feat: implement core typing logic

--- src/components/TypingArea.tsx
+++ src/components/TypingArea.tsx
@@ -1,16 +1,27 @@
 import { useState } from "react"
 
 
+
 function TypingArea() {
 
     const [userInput, setUserInput] = useState('')
 
     const paragraphs: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     const characters: string[] = paragraphs.split('')
+    
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserInput(e.target.value)
+    }
 
     return (
         <>
-            <input className="hidden-input" type="hidden" value={userInput}></input>
+            <input
+                className="hidden-input"
+                type="text" 
+                value={userInput}
+                onChange={handleInputChange}
+                autoFocus
+            ></input>
 
             <div className="typing-area" onClick={() => {
                 const input = document.querySelector('.hidden-input') as HTMLInputElement
